1be0d15fc40900240d621313894bcc4ce7d1f6e6
Philip Goddard <pmgoddard89@gmail.com>
2017-09-20 22:26:37 +0100

    pruning planned, need to implement

--- tree.py
+++ tree.py
@@ -4,24 +4,15 @@ from sklearn import datasets
 
 
 class Node(object):
-    '''
-    represent a node with:
-    node index
-    left child index
-    right child index
-    score
-    split feature
-    split value
-    could add some sensible error checking (some values must be initialised)
-    could add getter / setter for child ix?
-    '''
 
-    def __init__(self, index, depth, parent, is_leaf, left_child_ix,
-                 right_child_ix, split_feature, split_value, metric_score,
+    def __init__(self, index, depth, parent, direction,
+                 is_leaf, left_child_ix, right_child_ix,
+                 split_feature, split_value, metric_score,
                  class_counts):
         self.index = index
         self.depth = depth
         self.parent = parent
+        self.direction = direction
         self.is_leaf = is_leaf
         self.left_child_ix = left_child_ix
         self.right_child_ix = right_child_ix
@@ -47,7 +38,7 @@ class Gini(object):
 
         return np.mean((gini_lt, gini_gt))
 
-    # Gini = 0 means pure, > 0 is not pure
+    # Gini = 0 means pure
     @staticmethod
     def score_improved(old_val, new_val):
         if old_val is None:
@@ -72,40 +63,15 @@ class Tree(object):
         self.metric = metric
         self.nodes = []
         self.node_index_counter = 0
+        self.is_grown = False
 
 
-    def __repr__(self):
-        # can we add direction as a node property? i.e. to denote if a left or
-        # a right child
-        out = ''
-        for node in self.nodes:
-            print()
-            if node.is_leaf:
-                print('LEAF', node.class_counts)
-                tmp_node = node
-                while True:
-
-                    tmp_parent = tmp_node.parent
-                    if tmp_parent is None:
-                        break
-                    print(self.nodes[tmp_parent].split_feature)
-                    print(self.nodes[tmp_parent].split_value)
-                    print(self.nodes[tmp_parent].class_counts)
-                    tmp_node = self.nodes[tmp_parent]
-
-
-        # what we want is a nicely formatted set of rules to be displayed!!
-        return None#str([(x.left_child_ix, x.right_child_ix, x.class_counts, x.split_feature) for x in self.nodes])
-
-
-    def grow_tree(self, df, features, target):
+    def grow_tree(self, df, features, target, prune = True):
         '''
-
+        assumes classes are labeled 0:(n classes-1)
         '''
-        # SOME ERROR CHECKING - are targets correctly labeled, e.g 0:n-1?
-
         # recursive partition
-        self.n_features = df[target].value_counts().shape[0]
+        self.n_cls = df[target].value_counts().shape[0]
         self.__rpart(df, features, target, depth=0, parent=None)
 
         # add in child nodes by inspecting parents
@@ -120,21 +86,29 @@ class Tree(object):
                 self.nodes[i].left_child_ix = LC
                 self.nodes[i].right_child_ix = RC
 
+        if prune:
+            self.__prune()
 
-    def __rpart(self, df, features, target, depth=0, parent=None):
+        self.is_grown = True
+
+
+    def __rpart(self, df, features, target, depth=0, parent = None, direction = None):
         '''
         Child indices will be added after recursion is finished
         '''
 
         # population proportions at each node
-        class_counts = np.bincount(df[target].values, minlength = self.n_features)
+        class_counts = np.bincount(df[target].values, minlength = self.n_cls)
 
         if depth > self.max_depth:
             # LEAF
-            self.nodes.append(Node(index=self.node_index_counter, depth = depth,
+            grp = df.loc[:, target].values
+            metric_score = self.metric(grp, grp)
+            self.nodes.append(Node(index = self.node_index_counter, depth = depth,
                                    metric_score = None, split_feature = None,
-                                   split_value=None, is_leaf=True,
-                                   parent=parent, class_counts = class_counts,
+                                   split_value = None, is_leaf = True,
+                                   parent=parent, direction = direction,
+                                   class_counts = class_counts,
                                    left_child_ix = None, right_child_ix = None))
             self.node_index_counter +=1
             return
@@ -172,10 +146,16 @@ class Tree(object):
                     split_feature = f
                     split_value = s
 
+        if not split:
+            # its a leaf, whats the purity of the leaf?
+            grp = df.loc[:, target].values
+            metric_score = self.metric(grp, grp)
+
         self.nodes.append(Node(index = self.node_index_counter, depth = depth,
                                metric_score = metric_score, split_feature = split_feature,
                                split_value = split_value,is_leaf = is_leaf,
-                               parent = parent, class_counts = class_counts,
+                               parent = parent, direction = direction,
+                               class_counts = class_counts,
                                left_child_ix = None, right_child_ix = None))
 
         if not split:
@@ -188,9 +168,21 @@ class Tree(object):
         self.node_index_counter += 1
 
         # Left child
-        self.__rpart(df[df[split_feature] <= split_value], features, target, depth, parent)
+        direction = 'L'
+        self.__rpart(df[df[split_feature] <= split_value], features, target, depth, parent, direction)
         # Right child
-        self.__rpart(df[df[split_feature] > split_value], features, target, depth, parent)
+        direction = 'R'
+        self.__rpart(df[df[split_feature] > split_value], features, target, depth, parent, direction)
+
+
+    def __prune(self):
+        # VERY simple pruning scheme- only criteria is that purity is not improved
+        # between child and parent
+        # build queue of leaves
+        # process
+        # if prune, add parent to back of queue
+        # once queue empty, break
+        pass
 
 
     def predict(self, feature_dict, predict_proba = False):
@@ -199,6 +191,9 @@ class Tree(object):
 
         Given new user, will output recommendations
         '''
+        if not self.is_grown:
+            raise Exception('tree not grown, cannot predict')
+
         curr_node = 0
 
         while True:
@@ -218,6 +213,46 @@ class Tree(object):
                 curr_node = self.nodes[curr_node].right_child_ix
 
 
+
+
+    def get_rules(self):
+        # can we add direction as a node property? i.e. to denote if a left or
+        # a right child
+        # Then a list of strings that represent each rule
+        # TAKE OUT OF __repr__ and call get_rules(self) - check to see if initialised then away we go
+        #
+
+        if not self.is_grown:
+            raise Exception('tree not grown, cannot predict')
+
+        rules = []
+        out = ''
+        for node in self.nodes:
+            print()
+            if node.is_leaf:
+                rule = []
+                rule.append(['class:' + str(node.class_counts.argmax())])
+                print('LEAF', node.class_counts)
+                tmp_node = node
+                while True:
+
+                    tmp_parent = tmp_node.parent
+                    if tmp_parent is None:
+                        break
+                    direction = (lambda x: '<=' if x == 'L' else '>')(tmp_node.direction)
+                    split_feature = self.nodes[tmp_parent].split_feature
+                    split_value = self.nodes[tmp_parent].split_value
+                    rule.append([ split_feature + direction + str(split_value)])
+
+                    tmp_node = self.nodes[tmp_parent]
+                rule = rule[::-1]
+                rules.append(rule)
+
+
+        # what we want is a nicely formatted set of rules to be displayed!!
+        return rules
+
+
 # We could create a TreeRF class by inheritance. This could have extra
 # beaviour and attributes, eg store performance on oob samples
 
@@ -231,40 +266,71 @@ class RandomForest(object):
         self.metric = metric
         self.trees = []
 
+
     def grow_trees(self, df, features, target):
         '''
-        keep out of bag
+        as per brieman's specification, we take bootstrap sample of
+        data, and randomly select features to ensure trees arent correlated
+        dont prune!
+        df: input pandas dataframe
+        features: list of feature names (string)
+        target: target name (string\)
         '''
+        self.n_cls = df[target].value_counts().shape[0]
+
         # Growing forest is an obvious candidate for parallel processing
         for i in range(self.ntrees):
             print('growing tree', i)
-            tree = Tree(self.max_depth, self.min_grp_size, self.metric)
+            tree = Tree(self.max_depth, self.min_grp_size, self.metric, prune = False)
 
             # bootstrap sample of data
             df_ix = df.index.values
             bag_ix = np.random.choice(df_ix, size = df_ix.shape[0], replace = True)
-            # simple extension is to analyse performance on oob
-            # we will skip this for brevety.
-            oob_ix = np.setdiff1d(df_ix, np.unique(bag_ix))
+            oob_ix = np.setdiff1d(df_ix, np.unique(bag_ix)) # not actually using this atm
 
             # randomly select mtry features
             feature_ix = np.random.choice(np.arange(len(features)), self.mtry, replace = False)
             curr_features = [features[x] for x in feature_ix]
 
-            # grow tree
+            # initialise tree
             tree = Tree(max_depth = self.max_depth,
                         min_grp_size = self.min_grp_size,
                         metric = self.metric
                         )
-
+            # grow tree
             tree.grow_tree(df.loc[bag_ix, :], curr_features, target)
 
             # add tree to forest
             self.trees.append(tree)
 
 
-    def predict(self):
-        raise NotImplementedError
+    def predict(self, feature_dict, predict_proba = False):
+        '''
+        predict outcome for new sample
+        This isnt vectorised, takes one sample at a time
+        '''
+        votes = np.zeros(self.n_cls)
+        for tree in self.trees:
+            curr_node = 0
+
+            while True:
+                if tree.nodes[curr_node].is_leaf:
+                    tmp = tree.nodes[curr_node].class_counts
+                    votes += tmp / np.sum(tmp)
+                    break
+
+                curr_split_feature = tree.nodes[curr_node].split_feature
+                curr_split_value = tree.nodes[curr_node].split_value
+
+                if feature_dict[curr_split_feature] <= curr_split_value:
+                    curr_node = tree.nodes[curr_node].left_child_ix
+                else:
+                    curr_node = tree.nodes[curr_node].right_child_ix
+
+        if predict_proba:
+            return votes / self.ntrees
+        else:
+            return votes.argmax()
 
 
 iris = datasets.load_iris()
@@ -279,5 +345,8 @@ rpart.grow_tree(df = iris_df, features = ['pl','pw','sl','sw'], target = 'specie
 tst = iris_df.loc[1,: ][:4].to_dict()
 rpart.predict(tst)
 
-rf = RandomForest(ntrees= 10, max_depth= 4, mtry = 2, min_grp_size= 10, metric = Gini())
-rf.grow_trees(df = iris_df, features = ['pl','pw','sl','sw'], target = 'species')
+# rf = RandomForest(ntrees= 10, max_depth= 4, mtry = 2, min_grp_size= 10, metric = Gini())
+# rf.grow_trees(df = iris_df, features = ['pl','pw','sl','sw'], target = 'species')
+
+
+# TODO: plot decision boundaries, make sure is sensible
